9155fcabd39683f58225c8f05e2d6c9601e0c33e
Hinds, Alex Joseph <ahinds9@gatech.edu>
2025-06-10 21:07:13 -0500

    remove unused function and cleanup docstring

--- GBSeperation/remove_leaves.py
+++ GBSeperation/remove_leaves.py
@@ -10,39 +10,7 @@ from .Accuracy_evaluation import evaluate_indicators
 from .Visualization import show_graph, sp_graph, show_pcd
 import laspy
 import matplotlib.pyplot as plt
-
-
-def load_point_cloud(file_path, intensity_threshold = 0, full_data = False):
-    """
-    Load a point cloud from LAS or LAZ files.
-
-    Parameters:
-    file_path : str
-        Path to the LAS or LAZ file.
-
-    Returns:
-    point_cloud : ndarray
-        Nx3 matrix of point coordinates (x, y, z).
-    """
-    if ".xyz" in file_path:
-        # Load point cloud from an XYZ file
-        point_data = np.loadtxt(file_path, dtype=np.float64)
-        if point_data.shape[1] == 3:
-            point_cloud = point_data
-        elif point_data.shape[1] == 4:
-            I = point_data[:, 3] > intensity_threshold
-            point_cloud = point_data[I, :3]
-        else:
-            raise ValueError("Unsupported format in XYZ file.")
-        return point_cloud if not full_data else (point_cloud, point_data)
-    with laspy.open(file_path) as las:
-        point_data = las.read()
-        point_data = np.vstack((point_data.x, point_data.y, point_data.z,point_data.intensity)).T.astype('float64')
-        I = point_data[:,3]>intensity_threshold
-        point_data = point_data[I]
-        point_cloud = point_data[:,0:3]
-    return point_cloud if not full_data else (point_cloud,point_data)
-
+from Utils.Utils import load_point_cloud
 
 class LeafRemover:
     def __init__(self):
@@ -52,13 +20,13 @@ class LeafRemover:
         
     def process(self, xyz_point_cloud):
         """
-        From GBSeperation (https://zenodo.org/records/6837613)
+        Adapted from GBSeperation (https://zenodo.org/records/6837613)
         
         Parameters: 
-            xyzi_point_cloud: Nx4 array of XYZ and intensity values
+            xyz_point_cloud: Nx3 array of XYZ and intensity values
         Returns: 
-            wood: Nx4 array of XYZ and intensity values representing wood
-            leaf: Nx4 array of XYZ and intensity values representing leaves
+            wood: Nx3 array of XYZ and intensity values representing wood
+            leaf: Nx3 array of XYZ and intensity values representing leaves
         """
         # Please ensure that the growth direction of the tree is parallel to the Z coordinate axis.
 
